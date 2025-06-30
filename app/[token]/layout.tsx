f395500c35836a30e1d0e2d1899af7bb03383452
Ogin-ogin <ginga.m@icloud.com>
2025-06-30 15:00:46 +0900

    paramsをpropsから削除

--- app/[token]/layout.tsx
+++ app/[token]/layout.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Navigation from '../../components/navigation';
 
-export default function TokenLayout({ children, params }: { children: React.ReactNode; params: { token: string } }) {
+export default function TokenLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navigation token={params.token} />
+      <Navigation />
       <main className="max-w-4xl mx-auto p-4">{children}</main>
     </div>
   );
