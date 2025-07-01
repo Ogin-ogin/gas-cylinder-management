88d4f2bd129fe1810097537e6319670d1ed59e59
Ogin-ogin <ginga.m@icloud.com>
2025-07-01 14:59:43 +0900

    エラー修正

--- app/[token]/qr-codes/page.tsx
+++ app/[token]/qr-codes/page.tsx
@@ -1,4 +1,5 @@
-// ...QRコード管理ページ雛形...
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { Card } from "../../../components/ui/card";
 import { Badge } from "../../../components/ui/badge";
